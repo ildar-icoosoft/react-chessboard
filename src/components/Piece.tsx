c9cd48ff87b907b0a645698a5807a9a4211de41f
ildar <ildar.icoosoft@gmail.com>
2020-06-02 01:43:15 +0500

    interm

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -52,7 +52,7 @@ export const Piece: FC<PieceProps> = ({
     if (transitionFrom) {
       setInProp(true);
     }
-  });
+  }, []);
 
   return (
     <Transition in={inProp} timeout={transitionDuration}>
