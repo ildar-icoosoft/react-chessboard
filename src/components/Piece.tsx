cf3301c1d2225421a83a42a0133073baa814961b
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-06-01 23:15:14 +0500

    Reset Piece move CSS transition styles when transition is finished (#1)

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, FC } from "react";
+import React, { CSSProperties, FC, useEffect, useState } from "react";
 import { PieceCode } from "../enums/PieceCode";
 import { getPieceElement } from "../utils/chess";
 import { Transition } from "react-transition-group";
@@ -27,10 +27,10 @@ const getTransitionStyles = (
   }
 
   const styles: PartialRecord<TransitionStatus, CSSProperties> = {
-    entering: {
+    exited: {
       transform: `translate(${transitionFrom.x}px, ${transitionFrom.y}px)`,
     },
-    entered: {
+    entering: {
       transform: "translate(0, 0)",
       transition: `transform ${transitionDuration}ms`,
       zIndex: 1,
@@ -46,8 +46,16 @@ export const Piece: FC<PieceProps> = ({
   transitionFrom,
   transitionDuration = DEFAULT_TRANSITION_DURATION,
 }) => {
+  const [inProp, setInProp] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (transitionFrom) {
+      setInProp(true);
+    }
+  });
+
   return (
-    <Transition appear={true} in={transitionFrom ? true : false} timeout={0}>
+    <Transition in={inProp} timeout={transitionDuration}>
       {(state) => {
         return (
           <div
