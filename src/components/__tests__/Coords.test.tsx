dfe5d46427ddb27dd376baa5ac0f46a3dc0ace9b
ildar <ildar.icoosoft@gmail.com>
2020-06-05 03:18:29 +0500

    interm

--- src/components/__tests__/Coords.test.tsx
+++ src/components/__tests__/Coords.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TestRenderer from "react-test-renderer";
 import "@testing-library/jest-dom/extend-expect";
 import { Coords } from "../Coords";
+import { render } from "@testing-library/react";
 
 jest.useFakeTimers();
 
@@ -9,5 +10,17 @@ describe("Square", () => {
   it("Snapshot", () => {
     const tree = TestRenderer.create(<Coords />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe("DOM structure", () => {
+    it("should contain data-testid coords-ranks", () => {
+      const { queryByTestId } = render(<Coords />);
+      expect(queryByTestId("coords-ranks")).toBeInTheDocument();
+    });
+
+    it("should contain data-testid coords-files", () => {
+      const { queryByTestId } = render(<Coords />);
+      expect(queryByTestId("coords-files")).toBeInTheDocument();
+    });
   });
 });
