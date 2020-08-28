44260ab51829d03765ea92997fd9b19e5dfcdcfb
ildar <ildar.icoosoft@gmail.com>
2020-08-28 11:10:41 +0500

    feat: REACTCHESSBOARD-31 install postcss url plugin

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,10 +1,16 @@
 const postcss = require("rollup-plugin-postcss");
+const url = require("postcss-url");
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
         modules: true,
+        plugins: [
+          url({
+            url: "inline",
+          }),
+        ],
       })
     );
     return config;
