8aff669734e84cbd35a593a6066d512cf9e5a0ee
ildar <ildar.icoosoft@gmail.com>
2020-09-06 14:31:02 +0500

    feat: add enums, types, interfaces to public API

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
 export * from "./components/Board";
+export * from "./enums/PieceColor";
+export * from "./interfaces/Move";
+export * from "./interfaces/Position";
+export * from "./types/ValidMoves";
